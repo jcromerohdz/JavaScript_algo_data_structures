e9f3d5e17140887772c959e4215e683fac4b8c35
jcromerohdz <jcromerohdz@gmail.com>
2022-01-31 10:26:54 -0800

    Multiple pointers practice average pair and is subsequence

---
+++ isSubsequence.js
@@ -0,0 +1,22 @@
+/*
+Write a function called isSubsequence which takes in two strings and checks whether the 
+characters in the first string form a subsequence of the character in the second string. In other
+words, the function should check whether the characters in the first string appear somewhere in 
+the second string, whithout their order changing.
+*/
+
+function isSubsequence(str1, str2){
+  let i = 0;
+  let j = 0;
+  if (!str1) return true;
+  while(j < str2.length){
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++
+  }
+  return false;
+}
+
+console.log(isSubsequence('hello', 'hello world')) //true
+console.log(isSubsequence('sing', 'sting')) //true
+console.log(isSubsequence('abc', 'acb')) //false
